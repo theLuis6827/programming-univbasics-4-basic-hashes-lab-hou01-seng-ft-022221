c33d14a007dcf31094b99571632ae8cee8679f9d
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-11 06:49:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,11 @@ def my_hash
 end
 
 def pioneer
-  {:name
+  {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
+  {}
   # return a hash with a key :id assigned to the provided number
 end
