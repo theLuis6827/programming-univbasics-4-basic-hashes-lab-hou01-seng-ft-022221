fbb39a67470ef34e64c6c4a9b7698c1f977464d9
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-11 06:48:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,13 @@
 def new_hash
-  new_hash = {}
+  {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  {audi => s4}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {:name
 end
 
 
